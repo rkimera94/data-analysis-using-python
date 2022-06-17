fde840cc14e88f475ef8488fe358622821f03d0f
rkimera <rkimera2013@gmail.com>
2022-06-17 12:15:49 +0300

    add slqlit database

--- unitTest/app.py
+++ unitTest/app.py
@@ -4,9 +4,21 @@ import sqlite3 as lite
 class ShoppingChart(object):
     def __init__(self, item):
         self.item = item
+        global cur
+        try:
+            con = lite.connect('shopping_cart.db')
+            cur = con.cursor()
+        except Exception:
+            print('unable to create database')
 
     def addItem(self):
         print(self.item)
+
+    def fetchItem(self):
+        pass
+
+    def deleteItem(self):
+        pass
 
 
 item = ['apple', 'banana']
